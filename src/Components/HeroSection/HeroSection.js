3927ea0779e0ec984a58bc6583fe8a3862296d9f
favour <f.ebebeinwe@gmail.com>
2021-11-29 09:33:24 +0100

    HeroSection

---
+++ src/Components/HeroSection/HeroSection.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import {
+    HeroContainer,
+    HeroBg, VideoBg, 
+    HeroContent, 
+    HeroH1,
+    HeroP, 
+    HeroBtnWrapper,  
+    ArrowForward, 
+    ArrowRight
+} from './HeroElement'
+import {Button} from '../ButtonElement';
+import Video from '../../videos/video1.mp4'
+
+const HeroSection = () => {
+
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    } 
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Made Easy</HeroH1>
+                <HeroP>Sign up for a new account today and recieve $250 in credit towards your next payment.</HeroP>
+                <HeroBtnWrapper>
+                    <Button to='signup' onMouseEnter={onHover} OnMouseLeave={onHover}
+                        primary="true"
+                        dark="true"
+                    >
+                        Get started{hover ? <ArrowForward/> : <ArrowRight/>}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import Navbar from '../Components/Navbar/Navbar';
 import Sidebar from '../Components/Sidebar/Sidebar'
+import HeroSection from '../Components/HeroSection/HeroSection';
+import InfoSection from '../Components/InfoSection/InfoSection';
 const Home = () => {
     const[isOpen, setIsOpen] = useState(false)
 
@@ -11,6 +13,8 @@ const Home = () => {
         <div>
             <Sidebar isOpen={isOpen} toggle={toggle}/>
             <Navbar toggle={toggle}/>
+            <HeroSection/>
+            <InfoSection/>
         </div>
     )
 }
